3e3cc6406bd1e8933868aa87c08f6f4ccc2cbbd9
Lucas Floriano da Silva <112184116+lucasflorianodev@users.noreply.github.com>
2023-07-23 07:24:12 -0300

    Commit 004
    Adição de modal nos cards

--- MB - Advogados/assets/script.js
+++ MB - Advogados/assets/script.js
@@ -8,3 +8,19 @@ function menuShow() {
         document.querySelector('.icon').src = "/imgs/close_white_36dp.svg.svg";
     }
 }
+
+/*modal 1*/
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+})
